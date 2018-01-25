9b9b28a5bde50209735b1dd532e7a8ee6373d73f
Yi Yue <roy.yi.yue@gmail.com>
2018-01-25 13:21:34 -0800

    Add Axes

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ const data = [
   { name: 'David', math: 64, science: 76 },
   { name: 'Emily', math: 80, science: 94 }
 ]
+const margin = { top: 10, right: 10, bottom: 20, left: 50 }
 // specify chart dimensions 
-const width = 400
-const height = 200
+const width = 600 - margin.left - margin.right
+const height = 400 - margin.top - margin.bottom
+
 // create a scale to map scores to bar widths 
 // test scores are stored as percentages 
 // a score of 100 should create a full-width bar 
@@ -39,6 +41,23 @@ class App extends Component {
   }
   componentDidMount() {
     this.draw()
+    const svg = d3.select('#chart')
+      .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .style('position', 'absolute')
+      .style('top', 115)
+      .style('left', 0)
+    
+    const axisContainer = svg.append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+    axisContainer.append('g')
+      .attr('transform', `translate(0, ${height})`)
+      .call(d3.axisBottom(xScale))
+
+    axisContainer.append('g')
+      .call(d3.axisLeft(yScale))
   }
   componentDidUpdate() {
     this.draw()
@@ -58,7 +77,7 @@ class App extends Component {
       .append('div') // to add new bars for any data items without an existing DOM element 
       .attr('class', 'bar')
       .style('width', 0) // set the initial width to 0 
-
+      
     // combine the selections of new and existing bars 
     // so you can act on them together 
     newBars.merge(bars)
@@ -70,7 +89,7 @@ class App extends Component {
         // pass the score through the linear scale function 
         return xScale(d[this.state.subject]) + 'px'
       })
-      .style('height', d => yScale.bandwidth() + 'px')
+      .style('height', d => yScale.bandwidth() - 2.5 + 'px')
   }
 }
 
