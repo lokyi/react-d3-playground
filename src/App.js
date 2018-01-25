7ae00a9bb30c2fb19b992a1f327c0325ef991d22
Yi Yue <roy.yi.yue@gmail.com>
2018-01-25 00:36:24 -0800

    Animated

--- src/App.js
+++ src/App.js
@@ -29,23 +29,44 @@ class App extends Component {
     this.draw()
   }
   componentDidUpdate() {
-    d3.select('#chart') // within the #chart container... 
-      .selectAll('div') // select all the divs 
-      .remove()         // and remove them, clearing the #chart div 
+    // d3.select('#chart') // within the #chart container... 
+    //   .selectAll('div') // select all the divs 
+    //   .remove()         // and remove them, clearing the #chart div 
     this.draw()
   }
   handleClick = (subject) => {
     this.setState({ subject })
   }
   draw = () => {
-    d3.select('#chart')
-      .selectAll('div')
-      .data(data) // pair each number in the array with an empty div 
-      .enter()
-      .append('div')
-      .attr('class', 'bar') // color, height, and spacing via CSS 
+    // d3.select('#chart')
+    //   .selectAll('div')
+    //   .data(data, d => d.name) // pair each number in the array with an empty div 
+    //   .enter()
+    //   .append('div')
+    //   .attr('class', 'bar') // color, height, and spacing via CSS 
+    //   .transition() // animate everything that comes after this line! 
+    //   .style('width', d => {
+    //     return d[this.state.subject] + 'px' // use the subject passed in to grab the correct property  
+    //   })
+    
+    // store a reference to the bars already on the chart 
+    const bars = d3.select('#chart')
+      .selectAll('div') // this won't be empty after the first time this function runs 
+      .data(data, d => {
+        return d.name // use the name property to match across updates 
+      })
+
+    const newBars = bars.enter() // use the enter selection 
+      .append('div') // to add new bars for any data items without an existing DOM element 
+      .attr('class', 'bar')
+      .style('width', 0) // set the initial width to 0 
+
+    // combine the selections of new and existing bars 
+    // so you can act on them together 
+    newBars.merge(bars)
+      .transition() // animate everything that comes after this line! 
       .style('width', d => {
-        return d[this.state.subject] + 'px' // use the subject passed in to grab the correct property  
+        return d[this.state.subject] + 'px' // set the width like normal! 
       })
   }
 }
